8c36aeb2bc319546cb0a2b118170e61dc0775d80
Henry <hdr0006@auburn.edu>
2020-04-20 00:07:35 -0500

    Update cam.js
    jnhkjh

--- cam.js
+++ cam.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
         player.srcObject = stream;
     };
 
-    
+
     navigator.mediaDevices.getUserMedia({ video: true })
         .then(handleSuccess)
 
@@ -24,10 +24,11 @@ document.addEventListener('DOMContentLoaded', function () {
     canvas.height = ch;
 
     player.addEventListener('play', function () {
+        captureBackground();
         draw(this, context, dim, dim);
     }, false);
 
-    
+
 
 }, false);
 
@@ -46,12 +47,12 @@ async function draw(v, c, w, h) {
     // }
 
     let img = tf.browser.fromPixels(frame).resizeNearestNeighbor([dim,dim]).toFloat();
-    
 
+    let scale = tf.scalar(255.0)
     // Load model and predict what img is
     const model = await tf.loadLayersModel('http://127.0.0.1:8080/model.json');
     let img_reshape = img.reshape([-1, dim, dim, 3]);
-    const prediction = await model.predict(img_reshape).argMax([-1]);
+    const prediction = await model.predict(img_reshape.div(scale).sub(bgImageData)).argMax([-1]);
     console.log(prediction.print());
 
     // // Takes the probability from the prediction and converts it to percentage
@@ -65,7 +66,7 @@ async function draw(v, c, w, h) {
     // Displays the image that is being input to the model on the top left of the screen
     c.putImageData(frame, 0, 0);
 
-    
+
 
     // Calls the function again after certain amount of time (in ms)
     setTimeout(draw, 400, v, c, w, h);
@@ -81,7 +82,7 @@ function capture() {
     document.getElementById("countdown").textContent = "Get Ready";
     var captureFrames = setInterval(function () {
         timeleft--;
-        
+
         if (timeleft == 0) {
             document.getElementById("countdown").textContent = "Finished";
             console.log(preds);
@@ -90,20 +91,20 @@ function capture() {
             document.getElementById("countdown").textContent = "Capturing " + timeleft;
             preds.push(className);
             probs.push(prob);
-            
-            
+
+
 
         }
-        
-        
-        
+
+
+
         if (timeleft <= 0)
             //console.log(preds);
             clearInterval(captureFrames);
     }, 600);
-    
-    
-    
+
+
+
 }
 
 // Sets the bgImageData variable
@@ -112,12 +113,13 @@ function captureBackground() {
     var canvas = document.getElementById('c');
     var context = canvas.getContext('2d');
 
-    context.drawImage(player, 0, 0, 224, 224);
-    bgImageData = context.getImageData(0, 0, 224, 224);
 
-    
-    
+    // Gets the image from the webcam and converts it to an input for the tensorflow model
+    let framee = context.getImageData(0, 0, 240, 240);
+    let ff = tf.scalar(255.0);
+    context.drawImage(player, 0, 0, 240, 240);
+    bgImageData = tf.browser.fromPixels(framee).resizeNearestNeighbor([dim,dim]).toFloat().div(ff);
+
+
+
 }
-
-
-
